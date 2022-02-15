1dae543e6297087998840b4ab9ea07dfed1dea3e
Show Flix <83741010+showflix@users.noreply.github.com>
2022-02-15 07:36:36 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -39,7 +39,7 @@ async def clone_medias(client: Bot, message: Message):
     fn_caption = bool(query.file_caption)
     #
     if bool(clone_delay):
-        delay = 120
+        delay = 240
     else:
         delay = 0.25
     #
